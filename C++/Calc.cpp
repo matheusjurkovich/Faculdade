d36d37323a760282ab8dfd8dd401efe52d13c2ab
matheusjurkovich <matheusjurkovich@gmail.com>
2023-08-23 14:58:33 -0300

    Calculadora remasterizada

--- C++/Calc.cpp
+++ C++/Calc.cpp
@@ -3,30 +3,44 @@ using namespace std;
 
 int main()
 {
-    int a, b;
+    cout << "======= Calculadora =======" << endl << endl;
 
     while (true) 
     {
-    	cout << "Digite o primeiro numero: ";
-    cin >> a;
+        int a, b;
 
-    cout << "Digite o segundo numero: ";
-    cin >> b;
+        cout << "Digite o primeiro numero: ";
+        cin >> a;
 
-    cout << "Resultados:" << endl;
-    cout << a << " + " << b << " = " << a + b << endl;
-    cout << a << " - " << b << " = " << a - b << endl;
-    cout << a << " * " << b << " = " << a * b << endl;
+        cout << "Digite o segundo numero: ";
+        cin >> b;
 
-    if (b != 0)
-    {
-        cout << a << " / " << b << " = " << a / b << endl;
+        cout << "Resultados:" << endl;
+        cout << a << " + " << b << " = " << a + b << endl;
+        cout << a << " - " << b << " = " << a - b << endl;
+        cout << a << " * " << b << " = " << a * b << endl;
+
+        if (b != 0)
+        {
+            cout << a << " / " << b << " = " << static_cast<double>(a) / b << endl;
+        }
+        else
+        {
+            cout << "Divisao por zero. Numero invalido." << endl;
+        }
+
+        char opcao;
+        cout << "Deseja realizar outro calculo? (S/N): ";
+        cin >> opcao;
+
+        if (opcao != 'S' && opcao != 's')
+        {
+            cout << "Saindo da calculadora." << endl;
+            break;
+        }
+
+        cout << endl;
     }
-    else
-    {
-        cout << "Divisao por zero. Numero invalido." << endl;
-    }
-	}
 
     return 0;
 }
